c08d1196fd6948990c8c87f907d0a8fbc443ce64
Bless <artemstepantev@gmail.com>
2026-04-10 15:05:17 +0400

    feat: split code into modules

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,24 @@
-document.querySelectorAll('.course-card').forEach(card => {
-  card.addEventListener('click', () => {
-    const course = card.dataset.course;
-    window.location.href = `course-pagis/course-${course}.html`;
-  });
+import { questions } from "./questions/questions-html.js";
+import { renderQuestions } from "./renderQuestions.js";
+
+// запуск вопросов
+document.addEventListener("DOMContentLoaded", () => {
+  renderQuestions(questions);
 });
 
+// обработка карточек (если есть)
+const cards = document.querySelectorAll('.course-card');
+
+if (cards.length > 0) {
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      const course = card.dataset.course;
+      window.location.href = `course-pagis/course-${course}.html`;
+    });
+  });
+}
+
+// header
 const path = window.location.pathname.includes('course-pagis')
   ? '../components/header.html'
   : 'components/header.html';
