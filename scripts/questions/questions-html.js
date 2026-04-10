b701deeadca2617684ba2f9b16948546ae0f909e
Bless <artemstepantev@gmail.com>
2026-04-10 15:05:46 +0400

    feat: move questions to separate file

---
+++ scripts/questions/questions-html.js
@@ -0,0 +1,86 @@
+export const questions = [
+  {
+  question: "Какой тег используется для создания ссылки?",
+  correctAnswers: ["a", "<a>"]
+},
+{
+  question: "Какой атрибут указывает адрес ссылки в теге <a>?",
+  correctAnswers: ["href"]
+},
+{
+  question: "Какой тег используется для вставки изображения?",
+  correctAnswers: ["img", "<img>"]
+},
+{
+  question: "Какой атрибут обязателен для <img> для доступности?",
+  correctAnswers: ["alt"]
+},
+{
+  question: "Какой тег используется для ненумерованного списка?",
+  correctAnswers: ["ul", "<ul>"]
+},
+{
+  question: "Какой тег используется для нумерованного списка?",
+  correctAnswers: ["ol", "<ol>"]
+},
+{
+  question: "Какой тег используется для элемента списка?",
+  correctAnswers: ["li", "<li>"]
+},
+{
+  question: "Какой тег создает кнопку?",
+  correctAnswers: ["button", "<button>"]
+},
+{
+  question: "Какой тег используется для ввода данных пользователем?",
+  correctAnswers: ["input", "<input>"]
+},
+{
+  question: "Какой тег используется для формы?",
+  correctAnswers: ["form", "<form>"]
+},
+{
+  question: "Что делает тег <br>?",
+  correctAnswers: ["перенос строки", "перенос"]
+},
+{
+  question: "Является ли <br> парным тегом?",
+  correctAnswers: ["нет", "no"]
+},
+{
+  question: "Какой тег используется для самого важного заголовка?",
+  correctAnswers: ["h1", "<h1>"]
+},
+{
+  question: "Сколько всего уровней заголовков в HTML?",
+  correctAnswers: ["6"]
+},
+{
+  question: "Какой тег используется для параграфа?",
+  correctAnswers: ["p", "<p>"]
+},
+{
+  question: "Какой тег делает текст жирным С СМЫСЛОМ?",
+  correctAnswers: ["strong", "<strong>"]
+},
+{
+  question: "Какой тег просто делает текст жирным без смысла?",
+  correctAnswers: ["b", "<b>"]
+},
+{
+  question: "Какой тег используется для переноса строки без нового абзаца?",
+  correctAnswers: ["br", "<br>"]
+},
+{
+  question: "Какой тег используется для создания таблицы?",
+  correctAnswers: ["table", "<table>"]
+},
+{
+  question: "Какой тег используется для строки таблицы?",
+  correctAnswers: ["tr", "<tr>"]
+},
+{
+  question: "Какой тег используется для ячейки таблицы?",
+  correctAnswers: ["td", "<td>"]
+}
+];
